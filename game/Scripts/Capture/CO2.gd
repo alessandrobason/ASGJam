854c735564b2f9b3edf0bcc95d1214c909c8b4e1
1603405 <1603405@uad.ac.uk>
2021-07-15 14:40:02 +0100

    Please no flame

--- game/Scripts/Capture/CO2.gd
+++ game/Scripts/Capture/CO2.gd
@@ -1,16 +1,30 @@
 extends RigidBody2D
 
+# We want to create an upper limit to for the molecules velocity
+export var maxVelocity:=		Vector2(4000, 400)
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+# Rate at which the molecules will accelarate to max lift
+export var liftRate = 50
 
-
-# Called when the node enters the scene tree for the first time.
+# Constructor (kinda) 
 func _ready() -> void:
-	pass # Replace with function body.
+	# Set initial position
+	position = Vector2(5, 5)
 
+# the molecules will be ejected from the pipe with a 
+# horizontal impulse force this force will be dampened 
+# quickly after ejections so the movement of the molecules
+# are predominantly in the positive verticle axis
+	apply_central_impulse(Vector2(rand_range(500, maxVelocity.x), 0))
+	
+	# Apply lify
+	add_central_force(Vector2(0, liftRate))
+
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	
+
+	if linear_velocity.y >= maxVelocity.y:
+		add_central_force(Vector2(0, liftRate * delta))
